312c13609bb670d0981f2c0412fd987822504ffc
Cindy Nurul A <88866928+cindynurul@users.noreply.github.com>
2022-01-21 15:40:29 +0700

    hari ke 5

---
+++ dart-fundamental/hari ke 5/latihan/latihan1.dart
@@ -0,0 +1,16 @@
+void main() {
+  print("Looping ke 1");
+  var flag = 2;
+  while (flag <= 20) {
+    print(flag.toString() + " - i love koding");
+    // Mengubah nilai flag dengan menambahkan 1
+    flag += 2;
+  }
+  print("Looping ke 2");
+  var flag2 = 20;
+  while (flag2 >= 2) {
+    print(flag2.toString() + " - i will become a mobile developer");
+    // Mengubah nilai flag dengan menambahkan 1
+    flag2 -= 2;
+  }
+}
